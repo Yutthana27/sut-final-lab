166f7e58775e3782bc614e146f98a91682b6b403
Yutthana27 <yutthanasatorn5@gmail.com>
2026-01-07 11:25:49 +0700

    add task1 - close #1

---
+++ backend/entity/Books.go
@@ -0,0 +1,12 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	gorm.Model
+	Title string `valid:"stringlength(3|100)~Title error"`
+	Price float64 `valid:"range(50|5000)~Price must be between 50 and 5000"`
+	Code  string  `valid:"matches(^BK\\d{6}$)~“Code must start with BK followed by 6 digits (0-9)"`
+}
